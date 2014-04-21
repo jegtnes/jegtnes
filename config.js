65b7d21292975bdb3ec3c3d51b0fcedba1b00535
Alex Jegtnes <saccosekk@gmail.com>
2014-04-21 09:22:14 +0200

    Updated Ghost port

--- config.js
+++ config.js
@@ -66,7 +66,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '13834'
+            port: '10038'
         }
     },
 
