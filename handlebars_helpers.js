aa1f2f54cdc72ec5e4a762769b645bcc81f8a72c
Alex Jegtnes <saccosekk@gmail.com>
2014-05-01 20:25:18 +0100

    Attribution

--- handlebars_helpers.js
+++ handlebars_helpers.js
@@ -1,3 +1,9 @@
+// Dear Future Alex:
+// This was taken from:
+// https://ghost.org/forum/using-ghost/1773-custom-handlebars-helper/
+// So you know what the heck you're on about.
+// Love, Past Alex
+
 var hbs = require('express-hbs');
 var config = require('config');
 registerHelper = function (){
